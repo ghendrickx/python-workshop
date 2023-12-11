45b681e55c1ea46272e74447ee68cfbfa0ec48a2
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-11 13:40:52 +0100

    Docstring improvements.

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,25 +10,49 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):
+def load_csv(filename: str) -> np.ndarray:
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
+    :type filename: str
+
+    :return: Inflammation data
+    :rtype: numpy.ndarray
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+def daily_mean(data: np.ndarray) -> np.ndarray:
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: Inflammation data
+    :type data: numpy.ndarray
+
+    :return: Daily mean of inflammation data
+    :rtype: numpy.ndarray
+    """
     return np.mean(data, axis=0)
 
 
-def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+def daily_max(data: np.ndarray) -> np.ndarray:
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: Inflammation data
+    :type data: numpy.ndarray
+
+    :return: Daily maximum of inflammation data
+    :rtype: numpy.ndarray
+    """
     return np.max(data, axis=0)
 
 
-def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
-    return np.min(data, axis=0)
+def daily_min(data: np.ndarray) -> np.ndarray:
+    """Calculate the daily min of a 2D inflammation data array.
 
+    :param data: Inflammation data
+    :type data: numpy.ndarray
+
+    :return: Daily minimum of inflammation data
+    :rtype: numpy.ndarray
+    """
+    return np.min(data, axis=0)
