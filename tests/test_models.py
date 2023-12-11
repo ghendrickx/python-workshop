39e0c7eacca3ab67e8a95a81a4e3feff9b0d2a25
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-11 16:29:22 +0100

    Updated tests for patient normalisation

--- tests/test_models.py
+++ tests/test_models.py
@@ -46,33 +46,46 @@ def test_daily_min_string():
 
 
 @pytest.mark.parametrize(
-    'test, expected',
+    'test, expected, raising',
     [
         (
-                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None
         ),
         (
-                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
-                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None
         ),
         (
-                [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
-                [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None
         ),
         (
-                [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
-                [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            None
         ),
         (
-                [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
-                [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError
         ),
         (
-                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None
         )
     ]
 )
-def test_patient_normalise(test, expected):
-    npt.assert_almost_equal(normalise_patient(np.array(test)), np.array(expected), decimal=2)
+def test_patient_normalise(test, expected, raising):
+    if raising is None:
+        npt.assert_almost_equal(normalise_patient(np.array(test)), np.array(expected), decimal=2)
+    else:
+        with pytest.raises(raising):
+            npt.assert_almost_equal(
+                normalise_patient(np.array(test)), np.array(expected), decimal=2
+            )
+
