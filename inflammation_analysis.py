aed27d0d7d8b2b6b67b3df6114df2cdd772d0775
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-12 12:10:48 +0100

    Refactoring code

--- inflammation_analysis.py
+++ inflammation_analysis.py
@@ -15,12 +15,21 @@ def main(p_args: argparse.Namespace):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
+    # positional arguments
     in_files = p_args.infiles
     if not isinstance(in_files, list):
         in_files = [p_args.infiles]
 
+    # optional arguments
     on_screen: bool = p_args.onscreen
+    as_graph: bool = p_args.graph
 
+    # skip if no view is required
+    if (not on_screen) and (not as_graph):
+        print(f'Data not processed: Both visualisations disabled.')
+        return
+
+    # process and view data
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
@@ -32,16 +41,29 @@ def main(p_args: argparse.Namespace):
         }
 
         if on_screen:
-            print(f'\n{filename}:\n-------------------------')
-            print('\t'.join(view_data.keys()))
-            for v in zip(*view_data.values()):
-                print('\t'.join(map(str, np.round(v, 4))))
+            views.on_screen(view_data)
 
-        # views.visualize(view_data)
+        if as_graph:
+            views.visualize(view_data)
 
 
 if __name__ == "__main__":
+
+
     def str2bool(s: str) -> bool:
+        """Convert string-value to boolean:
+         -  True:   {'1', 't', 'y', 'true', 'yes', 'on'}
+         -  False:  {'0', 'f', 'n', 'false', 'no', 'off'}
+
+        :param s: string-representation
+        :type s: str
+
+        :return: boolean representation
+        :rtype: bool
+
+        :raises TypeError: if `s` is not a string
+        :raises ValueError: if `s` cannot be represented as a boolean
+        """
         if not isinstance(s, str):
             msg = f'Argument should be a string; {type(s)} given'
             raise TypeError(msg)
@@ -55,6 +77,7 @@ if __name__ == "__main__":
         msg = 'Invalid truth value'
         raise ValueError(msg)
 
+
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system'
     )
@@ -63,6 +86,12 @@ if __name__ == "__main__":
         'infiles',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient'
+    )
+    parser.add_argument(
+        '--graph',
+        default=True,
+        type=str2bool,
+        help='Output statistics as graph'
     )
     parser.add_argument(
         '--onscreen',
