760313ffbade5b7399275e4ce076dc02e72099f7
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-12 12:00:09 +0100

    Added on-screen representation of processed inflammation data

--- inflammation_analysis.py
+++ inflammation_analysis.py
@@ -3,19 +3,23 @@
 
 import argparse
 
+import numpy as np
+
 from inflammation import models, views
 
 
-def main(files):
+def main(p_args: argparse.Namespace):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    in_files = files.infiles
+    in_files = p_args.infiles
     if not isinstance(in_files, list):
-        in_files = [files.infiles]
+        in_files = [p_args.infiles]
+
+    on_screen: bool = p_args.onscreen
 
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
@@ -27,17 +31,45 @@ def main(files):
             'min': models.daily_min(inflammation_data)
         }
 
-        views.visualize(view_data)
+        if on_screen:
+            print(f'\n{filename}:\n-------------------------')
+            print('\t'.join(view_data.keys()))
+            for v in zip(*view_data.values()):
+                print('\t'.join(map(str, np.round(v, 4))))
+
+        # views.visualize(view_data)
 
 
 if __name__ == "__main__":
+    def str2bool(s: str) -> bool:
+        if not isinstance(s, str):
+            msg = f'Argument should be a string; {type(s)} given'
+            raise TypeError(msg)
+
+        s = s.lower()
+        if s in ('1', 't', 'y', 'true', 'yes', 'on'):
+            return True
+        elif s in ('0', 'f', 'n', 'false', 'no', 'off'):
+            return False
+
+        msg = 'Invalid truth value'
+        raise ValueError(msg)
+
     parser = argparse.ArgumentParser(
-        description='A basic patient inflammation data management system')
+        description='A basic patient inflammation data management system'
+    )
 
     parser.add_argument(
         'infiles',
         nargs='+',
-        help='Input CSV(s) containing inflammation series for each patient')
+        help='Input CSV(s) containing inflammation series for each patient'
+    )
+    parser.add_argument(
+        '--onscreen',
+        default=False,
+        type=str2bool,
+        help='Output statistics on screen'
+    )
 
     args = parser.parse_args()
 
